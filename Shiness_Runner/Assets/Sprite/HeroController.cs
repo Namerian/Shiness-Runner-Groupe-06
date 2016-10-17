f5fa39f1d36573a341570b563b29b7a643e3ffa7
ETU\t.monnet <t.monnet@rubika-edu.com>
2016-10-17 16:47:05 +0200

    Ajout HeroController

---
+++ Shiness_Runner/Assets/Sprite/HeroController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeroController : MonoBehaviour {
+
+    //public variables and properties
+    public float jumpHeight;
+    public float moveSpeed;
+    public bool isJumping
+    {
+        get
+        {
+            return isJumping;
+        }
+        set { }
+    }
+
+    //intern variables
+    Rigidbody _rb;
+    bool _moving;
+
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+
+    void Update()
+    {
+        if (_moving == true)
+        {
+            _rb.MovePosition(new Vector3(moveSpeed, _rb.velocity.y, 0));
+        }
+    }
+
+
+    public void Jump()
+    {
+        _rb.MovePosition(new Vector3(_rb.velocity.x, jumpHeight, 0));
+    }
+
+
+    public void StartMove()
+    {
+        _moving = true;
+    }
+
+
+    public void StopMove()
+    {
+        _moving = false;
+    }
+}
