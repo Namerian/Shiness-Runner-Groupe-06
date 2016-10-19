b4a1c81adae43aac5ec7daae3e135634066c0e06
Unknown <patrickmonville@hotmail.com>
2016-10-19 11:07:55 +0200

    work on input

--- Shiness_Runner/Assets/Script/GameState2d.cs
+++ Shiness_Runner/Assets/Script/GameState2d.cs
@@ -19,7 +19,27 @@ public class GameState2d : GameState
 
 	protected override void OnHandleInput (JoystickState[] joystickStates)
 	{
-	}
+        for (int i = 0; i < joystickStates.Length; i++)
+        {
+            JoystickState _stickState = joystickStates[i];
+            HeroController _character = gameManager.characters[i];
+
+            //
+            if (_character.gameObject.name != "Character" + (_stickState.characterIndex + 1))
+            {
+                Debug.LogError("GameState25d: OnHandleInput: character index (" + _character.gameObject.name + " != joystick index" + _stickState.characterIndex);
+            }
+
+            //jump
+            if (_stickState.buttonY_down)
+            {
+                _character.Jump();
+            }
+
+            //slide
+
+        }
+    }
 
 	protected override void OnUpdate ()
 	{
--- Shiness_Runner/Assets/Script/JoystickState.cs
+++ Shiness_Runner/Assets/Script/JoystickState.cs
@@ -3,21 +3,34 @@ using System.Collections;
 
 public class JoystickState
 {
-    public int CharacterIndex { get; private set; }
+    public int characterIndex { get; private set; }
 
-    public bool ButtonA { get; private set; }
-    public bool ButtonY { get; private set; }
-    public bool ButtonB { get; private set; }
-    public bool YAxisUp { get; private set; }
-    public bool YAxisDown { get; private set; }
+    public bool buttonA_down { get; private set; }
+    public bool buttonA_up { get; private set; }
 
-    public JoystickState(int characterIndex, bool buttonA, bool buttonY, bool buttonB, bool yAxisUp, bool yAxisDown)
+    public bool buttonY_down { get; private set; }
+    public bool buttonY_up { get; private set; }
+
+    public bool buttonB_down { get; private set; }
+    public bool buttonB_up { get; private set; }
+
+    public bool yAxisUp { get; private set; }
+    public bool yAxisDown { get; private set; }
+
+    public JoystickState(int characterIndex, bool buttonA_down, bool buttonA_up, bool buttonY_down, bool buttonY_up, bool buttonB_down, bool buttonB_up, bool yAxisUp, bool yAxisDown)
     {
-        CharacterIndex = characterIndex;
-        ButtonA = buttonA;
-        ButtonY = buttonY;
-        ButtonB = buttonB;
-        YAxisUp = yAxisUp;
-        YAxisDown = yAxisDown;
+        this.characterIndex = characterIndex;
+
+        this.buttonA_down = buttonA_down;
+        this.buttonA_up = buttonA_up;
+
+        this.buttonY_down = buttonY_down;
+        this.buttonY_up = buttonY_up;
+
+        this.buttonB_down = buttonB_down;
+        this.buttonB_up = buttonB_up;
+
+        this.yAxisUp = yAxisUp;
+        this.yAxisDown = yAxisDown;
     }
 }
