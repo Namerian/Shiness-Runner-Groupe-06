d134a207c9703f13483297a539c06ef7b0b863e0
ETU\n.pankowiak <n.pankowiak@rubika-edu.com>
2016-10-19 18:04:25 +0200

    New Extaz Bar + UI Work In Progress

---
+++ Shiness_Runner/Assets/Script/UI.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using DG.Tweening;
+
+public class UI : MonoBehaviour {
+
+    public Text scoretext;
+    public Text multiplier;
+    public Slider extaz;
+    public Image player1;
+    public Image player2;
+    public Image player3;
+
+    public float score;
+
+    // Use this for initialization
+    void Start () {
+        RotateScoreLeft();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        score = Mathf.Round(10 * Time.time * 1f) / 1f; ;
+        scoretext.text = "" + score;
+
+       
+
+    }
+
+    void RotateScoreLeft()
+    {
+        scoretext.transform.DOLocalRotate(new Vector3(0, 0, 4), 1.5f).SetId("RotateLeft").SetEase(Ease.InOutCubic).OnComplete(RotateScoreRight);
+    }
+
+    void RotateScoreRight()
+    {
+        scoretext.transform.DOLocalRotate(new Vector3(0, 0, -4), 1.5f).SetId("RotateRight").SetEase(Ease.InOutCubic).OnComplete(RotateScoreLeft);
+    }
+}
