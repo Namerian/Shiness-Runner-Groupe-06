80cda6a4bf00479273d19a280f13f116b80ad0dc
ETU\t.monnet <t.monnet@rubika-edu.com>
2016-10-21 00:20:10 +0200

    ajout script EndGame

---
+++ Shiness_Runner/Assets/Script/EndGame.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndGame : MonoBehaviour {
+
+    GameManager _gm;
+	
+	void OnTriggerEnter(Collider col)
+    {
+        _gm = FindObjectOfType<GameManager>();
+        _gm.SwitchState(new GameStateEndGame(_gm));
+    }
+}
