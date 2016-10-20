040ade3bc2fc459bc5d1ed73b24ed2ac41e8f1bc
ETU\t.monnet <t.monnet@rubika-edu.com>
2016-10-20 20:11:35 +0200

    degats en mode 2d fixed

--- Shiness_Runner/Assets/Script/GameStates/GameStateGameOver.cs
+++ Shiness_Runner/Assets/Script/GameStates/GameStateGameOver.cs
@@ -17,21 +17,17 @@ public class GameStateGameOver : GameState
 
     protected override void OnExit()
     {
-        throw new NotImplementedException();
     }
 
     protected override void OnHandleInput(JoystickState[] joystickStates)
     {
-        throw new NotImplementedException();
     }
 
     protected override void OnPlayerDied(PlayerInfo player)
     {
-        throw new NotImplementedException();
     }
 
     protected override void OnUpdate()
     {
-        throw new NotImplementedException();
     }
 }
